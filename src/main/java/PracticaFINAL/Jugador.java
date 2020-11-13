e223dcf150d96f3fee440efbb2bc5dd1651e3737
JordiCarrascoUAB <jordi.carrascos@e-campus.uab.cat>
2020-11-13 10:41:49 +0100

    proba

--- src/main/java/PracticaFINAL/Jugador.java
+++ src/main/java/PracticaFINAL/Jugador.java
@@ -4,6 +4,7 @@ public class Jugador {
 	private int player = 1;
 	private boolean winner=false;
 	
+	
 	public int getPlayer()
 	{
 		return player;
@@ -12,6 +13,6 @@ public class Jugador {
 	{
 		return winner;
 	}
-
+	
 	
 }
