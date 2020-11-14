0343b5f7504dba816f4b264afb98c48c0ad68475
JordiCarrascoUAB <jordi.carrascos@e-campus.uab.cat>
2020-11-14 16:48:09 +0100

    Correct Test constructor y getBoard

--- src/main/java/Controlador/Controlador.java
+++ src/main/java/Controlador/Controlador.java
@@ -8,8 +8,8 @@ public class Controlador {
 	private ConnectaQuatre board=null;
 	private Vistas vistas = new Vistas();
 	public Controlador(int amp,int alt) {
-		
+		this.board =new ConnectaQuatre(amp, alt);
 	}
-	public ConnectaQuatre getBoard() {return null;}
+	public ConnectaQuatre getBoard() {return board;}
 
 }
