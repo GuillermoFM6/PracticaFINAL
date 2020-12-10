734d2475cf224030a1c6eacf1625dfa58769b6b6
JordiDomingo9 <jordidomingo4@gmail.com>
2020-12-10 19:17:40 +0100

    Partidas para cada jugador

--- src/main/java/Model/MockKeyboardInput.java
+++ src/main/java/Model/MockKeyboardInput.java
@@ -12,20 +12,90 @@ public class MockKeyboardInput implements InterfazTeclado {
 	}
 	public MockKeyboardInput(int numero) {
 		switch(numero) {
-		
+		//Sempre inicia el mateix color.
 		case 1:
+			//Guanya amb horitzontals
 			jugadas.add(0);
+			jugadas.add(-1);
+			jugadas.add(0);
+			jugadas.add(1);
+			jugadas.add(1);
+			jugadas.add(5);
+			jugadas.add(2);
+			jugadas.add(2);
+			jugadas.add(3);
 
 			break;
 		case 2:
+			//Guanya amb verticals
+			jugadas.add(0);
+			jugadas.add(-1);
+			jugadas.add(1);
+			jugadas.add(0);
+			jugadas.add(5);
+			jugadas.add(2);
+			jugadas.add(0);
+			jugadas.add(3);
+			jugadas.add(0);
 			break;
 		case 3:
+			//Guanya amb diagonal
+			jugadas.add(0);
+			jugadas.add(-1);
+			jugadas.add(1);
+			jugadas.add(1);
+			jugadas.add(5);
+			jugadas.add(2);
+			jugadas.add(3);
+			jugadas.add(2);
+			jugadas.add(2);
+			jugadas.add(3);
+			jugadas.add(4);
+			jugadas.add(3);
+			jugadas.add(3);
 			break;
 		case 4:
+			//Guanya amb horitzontals
+			jugadas.add(4);
+			jugadas.add(0);
+			jugadas.add(-1);
+			jugadas.add(0);
+			jugadas.add(1);
+			jugadas.add(1);
+			jugadas.add(5);
+			jugadas.add(2);
+			jugadas.add(2);
+			jugadas.add(3);
 			break;
 		case 5:
+			//Guanya amb verticals
+			jugadas.add(4);
+			jugadas.add(0);
+			jugadas.add(-1);
+			jugadas.add(1);
+			jugadas.add(0);
+			jugadas.add(5);
+			jugadas.add(2);
+			jugadas.add(0);
+			jugadas.add(3);
+			jugadas.add(0);
 			break;
 		case 6:
+			//Guanya amb diagonal
+			jugadas.add(4);
+			jugadas.add(0);
+			jugadas.add(-1);
+			jugadas.add(1);
+			jugadas.add(1);
+			jugadas.add(5);
+			jugadas.add(2);
+			jugadas.add(3);
+			jugadas.add(2);
+			jugadas.add(2);
+			jugadas.add(3);
+			jugadas.add(4);
+			jugadas.add(3);
+			jugadas.add(3);
 			break;
 			
 		}
